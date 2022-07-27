31030a8be091496f787be174fb3c282459bd27fb
axel-lung <axel.lung@outlook.fr>
2022-07-27 20:45:45 +0200

    login system

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,64 @@
+import { AuthenticationService } from './../../services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  credentials: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthenticationService,
+    private alertController: AlertController,
+    private router: Router,
+    private loadingController: LoadingController
+  ) {}
+
+  ngOnInit() {
+    this.credentials = this.fb.group({
+      username: ['titi', [Validators.required, /*Validators.email*/]],
+      password: ['cityslicka', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  async login() {
+    console.log("test");
+    console.log(this.credentials.value);
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+
+
+    this.authService.login(this.credentials.value).subscribe(
+      async (res) => {
+        await loading.dismiss();
+        this.router.navigateByUrl('/loader', { replaceUrl: true });
+      },
+      async (res) => {
+        await loading.dismiss();
+        const alert = await this.alertController.create({
+          header: 'Login failed',
+          message: res.error.error,
+          buttons: ['OK'],
+        });
+
+        await alert.present();
+      }
+    );
+  }
+
+  // Easy access for form fields
+  get username() {
+    return this.credentials.get('username');
+  }
+
+  get password() {
+    return this.credentials.get('password');
+  }
+}
